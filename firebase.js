5da0593c3fb56b4f54a7a956963bcda4b2cbbc8f
LLENN03 <sunghanvid567@gmail.com>
2025-04-30 14:08:12 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// 📁 firebase.js
+// Firebase Admin SDK 초기화 모듈
+
+const admin = require('firebase-admin');
+
+// 도련님이 제공하신 service account 키 경로 또는 환경변수로부터 로딩
+const serviceAccount = require('./serviceAccountKey.json'); // 키 파일 이름이 이 파일과 동일해야 함
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin.firestore();
