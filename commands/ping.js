77968e88302f9667b4576bb407bed9de9c8ca6c9
LLENN03 <sunghanvid567@gmail.com>
2025-05-01 00:17:21 +0800

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Check bot latency'),
+  async execute(interaction) {
+    await interaction.reply(`🏓 Pong! Latency: ${Date.now() - interaction.createdTimestamp}ms`);
+  }
+};
